b327f9035a0ddcc7ca7f66d98854420e0df48a6f
Josh Gough <jsgough@gmail.com>
2014-04-05 15:11:40 -0400

    Fixed up basic operations and added some step instructions

--- server.js
+++ server.js
@@ -6,30 +6,35 @@ var msgs = [];
 
 app.use(express.urlencoded());
 
-/*
-// Convenience for allowing CORS on routes - GET only
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); 
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS'); 
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-*/
+function createResponse(newMsgs) {
+  var refreshSince = new Date();
+  if (newMsgs.length > 0) {
+    refreshSince = newMsgs[newMsgs.length-1].timestamp;
+  }
+  return {
+    msgs: newMsgs,
+    refreshSince: refreshSince
+  };
+}
 
-app.get('/msgs/:since?', function(req, res) {  
+app.get('/msgs/:since?', function(req, res) {
   if (!req.params.since) {
-    res.json(msgs);
+    res.json(createResponse(msgs));
   } else {
     var since = new Date(req.params.since);
-    console.log(since);
     var recentMsgs = [];
+    console.log('asking since:');
+    console.log(since);
+    console.log('actual most recent:');
+    if (msgs.length > 0) {
+      console.log(msgs[msgs.length-1].timestamp);
+    }
     msgs.forEach(function(msg) {
-      console.log(msg.timestamp);
       if (msg.timestamp > since) {
         recentMsgs.push(msg);
       }
     });
-    res.json(recentMsgs);
+    res.json(createResponse(recentMsgs));
   }
 });
 
@@ -37,7 +42,7 @@ app.post('/msgs', function(req, res) {
   var msg = req.body;
   msg.timestamp = new Date();
   msgs.push(msg);
-  res.json(msgs);
+  res.send("OK");
 });
 
 app.use(express.static(__dirname + '/public'));
@@ -47,3 +52,14 @@ var port = process.env.PORT || 80;
 app.listen(port, null, function (err) {
   console.log('Your chat server is listening at: http://localhost:' + port);
 });
+
+
+/*
+// Convenience for allowing CORS on routes - GET only
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS'); 
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+*/
